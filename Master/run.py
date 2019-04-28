5ea6ebe2576a9e8d4bdd765a8edce2cdb67f3563
AndrewZ <1599118717@qq.com>
2019-04-28 10:22:46 +0800

    4

--- Master/run.py
+++ Master/run.py
@@ -1,3 +1,8 @@
+import sys, os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append('../')
+
 from Master.Scheduler import MasterScheduler
 from Master.Until.tool import settings
 
